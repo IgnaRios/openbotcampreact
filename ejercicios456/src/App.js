4a89ac3a53ba7eaddc90f954aed560cea81b0782
IgnaRios <ignacio.rios@hotmail.es>
2022-11-16 05:24:54 -0300

    crear proyecto react ejercicios 456

---
+++ ejercicios456/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Clock from './components/classComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicios456/src/components/functionalComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FunctionalComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default FunctionalComponent;
