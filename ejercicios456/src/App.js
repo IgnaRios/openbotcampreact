c23a2dd6857e130c9feccfa1dba871081c8cb8f8
IgnaRios <ignacio.rios@hotmail.es>
2022-11-18 04:42:33 -0300

    crear boton para cambiar componentes class y funcional

--- ejercicios456/src/App.js
+++ ejercicios456/src/App.js
@@ -1,16 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
 import Clock from './components/classComponent';
 import FunctionalComponent from './components/functionalComponent';
 
 
 function App() {
+
+  const [state, setState] = useState(false);
+
   return (
     <div className="App">
-      {/*<Clock />*/}
-      <FunctionalComponent />
+
+      {state ? <Clock /> : <FunctionalComponent /> }
+       
+      <button onClick={cambiarComponente}>{state ? 'Cambiar componente componente funcioanl' : 'cambiar a componente clase'}</button>
     </div>
   );
-
+  
+  function cambiarComponente () {
+    setState(!state)
+  }
 
 }
 
--- ejercicios456/src/components/functionalComponent.jsx
+++ ejercicios456/src/components/functionalComponent.jsx
@@ -11,43 +11,31 @@ const FunctionalComponent = () => {
 
     const tick = () => {
         setPersona({
-           ...persona,
             fecha: new Date(),
-            edad : persona.edad + 1
-        });
+            edad : persona.edad + 1,
+            nombre:'Nacho',
+            apellido:'Ríos'
+        },);
     }
 
     useEffect(() => {
-       const timerID = () =>{
-            setInterval(
-                ()=> tick(), 1000
-            )
-       };
-       return(
-       timerID())
+       const timerID = setInterval( () => {
+            tick()}, 1000);
+        return() => {
+            clearInterval(timerID)
+        }       
     },);
 
     return (
         <div>
         <h2>
-         Hora Actual:
-         {persona.fecha.toLocaleTimeString()}
-         </h2>
-         <h3>{persona.nombre} {persona.apellido}</h3>
-         <h1>Edad: {persona.edad}</h1>
-         <button onClick={detenerReloj}>DETENER</button>  
+         Hora Actual: {persona.fecha.toLocaleTimeString()}
+        </h2>
+        <h3>{persona.nombre} {persona.apellido}</h3>
+        <h1>Edad: {persona.edad}</h1>  
         </div>
     );
     
-    function detenerReloj (){
-        clearInterval( () =>{
-            setPersona({
-                ...persona, 
-                fecha: new Date(),
-                edad : persona.edad
-            })
-        });
-    }
 }
 
 export default FunctionalComponent;
